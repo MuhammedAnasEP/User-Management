77fb2c3b633faf2a2659f7fd984e4cc38c765d43
MuhammedAnasEP <anasep1110@gmail.com>
2024-05-31 08:07:22 +0530

    Create model for storing users datas and connected to postgresql

--- core/settings.py
+++ core/settings.py
@@ -77,10 +77,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'user_management',
+        'USER': 'postgres',
+        'PASSWORD': 'p0stgres123',
+        'HOST': 'localhost',
     }
 }
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    company_name = models.CharField(max_length=255, blank=True)
+    age = models.IntegerField(blank=True, null=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip = models.IntegerField()
+    email = models.EmailField(unique=True)
+    web = models.URLField(blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
