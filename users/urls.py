3a69dba10af7af9b45601dca5053c36d5b03a323
MuhammedAnasEP <anasep1110@gmail.com>
2024-05-31 18:59:57 +0530

    create View for getting users details using id

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users', views.UsersList.as_view()),
+    path('users', views.UsersListView.as_view(), name='users-list'),
+    path('users/<int:id>', views.UserDetailsView.as_view(), name='user-details'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,16 +1,40 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
 from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.response import Response
+from rest_framework import status
 from .models import UserProfile
 from .serializers import UserProfileSerializer
 from .pagination import UserListPagination
 
 # Create your views here.
 
-class UsersList(ListAPIView):
+class UsersListView(ListAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     pagination_class = UserListPagination
     filter_backends = (OrderingFilter, SearchFilter)
     ordering_fields = '__all__'
     search_fields = ('first_name', 'last_name')
+
+class UserCreateView():
+    pass
+
+class UserDetailsView(APIView):
+    def get(self, request, id):
+        try:
+            user = UserProfile.objects.get(id = id)
+        except:
+            content = {'details':f'User with id {id} not availvable.'}
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = UserProfileSerializer(user, many=False)
+        return Response(serializer.data)
+    
+
+
+        
+        
+
+    
